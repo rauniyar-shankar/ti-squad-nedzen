12866861747b1be7841bb72a0d76bce218c188e1
rauniyar-shankar <shankarrauniyar99@gmail.com>
2026-05-29 17:38:04 +0530

    Delete app directory

--- app/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Bypassing the .env file and giving Next.js the exact strings
-const supabaseUrl = "https://vmyjjwqocrosplnylxft.supabase.co"
-const supabaseAnonKey = "sb_publishable_ZYJhUCKWxnqNv_L0nARWtA_jCPIAFxO"
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
